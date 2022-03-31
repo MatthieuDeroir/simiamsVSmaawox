fbd95763416c3a1534c846ead09830758604a3d8
MatthieuDeroir <matthieu.deroir@epsi.fr>
2022-03-31 16:55:12 +0200

    cleaning player class

--- src/Player.cpp
+++ src/Player.cpp
@@ -8,10 +8,7 @@
 Player::Player(string name, int hp = 100, int att = 1, int def = 1, int money = 0, char sprite = '.', int score = 0) {
     this->name = name;
     this->hp = hp;
-    this->att = att;
-    this->def = def;
     this->money = money;
-    this->sprite = sprite;
     this->score = score;
 }
 
@@ -33,36 +30,12 @@ void Player::setHp(int hp) {
     this->hp = hp;
 }
 
-int Player::getAtt() {
-    return this->att;
-}
-
-void Player::setAtt(int att) {
-    this->att = att;
-}
-
-int Player::getDef() {
-    return this->def;
-}
-
-void Player::setDef(int def) {
-    this->def = def;
-}
-
 int Player::getMoney() {
     return this->money;
 }
 
 void Player::setMoney(int money) {
     this->money = money;
-}
-
-char Player::getSprite() {
-    return this->sprite;
-}
-
-void Player::setSprite(char sprite) {
-    this->sprite = sprite;
 }
 
 int Player::getScore(){
@@ -75,8 +48,7 @@ void Player::setScore(int score){
 
 //To String
 void Player::toString() {
-    cout << "Player: " << this->name << "// HP: " << this->hp << "// ATT: " << this->att << "// DEF: " << this->def
-         << "// MONEY: " << this->money << "// SPRITE: " << this->sprite << endl;
+    cout << "Player: " << this->name << "// HP: " << this->hp << "// MONEY: " << this->money << endl;
 }
 // Methods
 
